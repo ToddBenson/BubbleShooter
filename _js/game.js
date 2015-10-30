9a27d27acb762a9b132daba611e423c00fe409d7
gl42ben <todd.benson@csaa.com>
2015-10-30 15:20:34 -0700

    New lines of code

--- _js/game.js
+++ _js/game.js
@@ -2,19 +2,27 @@ var BubbleShoot = window.BubbleShoot || {};
 BubbleShoot.Game = (function($){
   var Game = function(){
 	var curBubble;
+	var board;
+	var numBubbles;
+	var MAX_BUBBLES = 70;
 	this.init = function(){
 	  $(".but_start_game").bind("click",startGame);
 	};
 	var startGame = function(){
 		$(".but_start_game").unbind("click");
+		numBubbles = MAX_BUBBLES;
 		BubbleShoot.ui.hideDialog();
 		curBubble = getNextBubble();
+		board = new BubbleShoot.Board();
+		BubbleShoot.ui.drawBoard(board);
 		$("#game").bind("click",clickGameScreen);
 	};
 	var getNextBubble = function(){
 	        var bubble = BubbleShoot.Bubble.create();
 	        bubble.getSprite().addClass("cur_bubble");
 	        $("#board").append(bubble.getSprite());
+	        BubbleShoot.ui.drawBubblesRemaining(numBubbles);
+	        numBubbles--;
 	        return bubble;
 	};
 	var clickGameScreen = function(e){
@@ -29,6 +37,7 @@ BubbleShoot.Game = (function($){
 	        y : bubbleCoors.top - distY
 	    };
 	    BubbleShoot.ui.fireBubble(curBubble,coords,duration);
+	    curBubble = getNextBubble();
 	};
 	};
   return Game;
